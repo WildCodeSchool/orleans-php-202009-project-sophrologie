31bb1ca34919dab466f5f6ab3909d6375d754fe2
KevinVanphavong <kevin45van@hotmail.fr>
2020-12-04 12:29:17 +0100

    fix debug ini

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,16 @@ import '../styles/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(() => {
+    $('[data-toggle="popover"]').popover();
+});
