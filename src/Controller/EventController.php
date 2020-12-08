ab757a926c7ecd58f6a19694df4a184d82a10045
calouvts <pascalevetois@gmail.com>
2020-12-08 23:44:51 +0100

    create view event

---
+++ src/Controller/EventController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\EventRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EventController extends AbstractController
+{
+    /**
+     * @Route("/event", name="event")
+     */
+    public function index(EventRepository $eventRepository): Response
+    {
+        $events = $eventRepository->findAll();
+        return $this->render('event/index.html.twig', [
+                'events' => $events,
+        ]);
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    .addEntry('news', './assets/styles/news.scss')
+    .addEntry('event', './assets/styles/event.scss')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
