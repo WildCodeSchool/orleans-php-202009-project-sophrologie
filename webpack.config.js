4a886359e66ebd8460c2d307efc8733c8dde02d0
KevinVanphavong <kevin45van@hotmail.fr>
2020-12-08 20:56:35 +0100

    adding section welcome

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
+    .addEntry('home', './assets/styles/home.scss')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
