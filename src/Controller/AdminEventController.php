60c02cacb35dc46e7f99f63d8afd4b478d0ea5f0
calouvts <pascalevetois@gmail.com>
2020-12-13 01:00:30 +0100

    initial commit edit.

--- src/Controller/AdminEventController.php
+++ src/Controller/AdminEventController.php
@@ -11,10 +11,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/admin/event", name="admin_event_")
+ */
 class AdminEventController extends AbstractController
 {
+
     /**
-     * @Route("/admin/event/new", name="admin_new")
+     * @Route("/new", name="new")
      * @param Request $request
      * @param EntityManagerInterface $entityManager
      * @return Response
@@ -35,6 +39,28 @@ class AdminEventController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+    /**
+     * @Route("/edit/{id}", name="edit")
+     */
+    public function edit(Event $event, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_event_new');
+        }
+
+        return $this->render('admin_event/edit.html.twig', [
+            'form' => $form->createView(),
+            'event' => $event,
+        ]);
+    }
+
+
 
     /**
      * @Route("/admin/event/index", name="admin_event")
