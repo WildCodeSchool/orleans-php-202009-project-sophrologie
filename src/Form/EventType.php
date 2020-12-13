82c1f57e6540ab23b732cd0dfac8a21f114fd013
calouvts <pascalevetois@gmail.com>
2020-12-13 22:59:13 +0100

    add require false on form

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -24,18 +24,21 @@ class EventType extends AbstractType
             ])
             ->add('description', TextareaType::class, [
                 'label' => 'description',
+                'required' => false,
                 'attr' => [
                     'placeholder' => 'salon du bien être',
                 ],
             ])
             ->add('url', UrlType::class, [
                 'label' => 'lien de l\'image',
+                'required' => false,
                 'attr' => [
                     'placeholder' => 'https//image.jpeg',
                 ],
             ])
             ->add('date', DateTimeType::class, [
                 'label' => 'date de publication',
+                'required' => false,
             ])
             ->add('category', TextType::class, [
                 'label' => 'catégorie',
@@ -45,27 +48,32 @@ class EventType extends AbstractType
             ])
             ->add('summary', TextareaType::class, [
                 'label' => 'résumé',
+                'required' => false,
                 'attr' => [
                     'placeholder' => 'résumé de l\'article que vous souhaitez publier',
                 ],
             ])
             ->add('article', TextareaType::class, [
                 'label' => 'article',
+                'required' => false,
                 'attr' => [
                     'placeholder' => 'Vous pouvez saisir ici le texte de l\'article que vous souhaitez publier',
                 ],
             ])
             ->add('date', DateTimeType::class, [
                 'label' => 'date de la manifestation',
+                'required' => false,
             ])
             ->add('eventlink', UrlType::class, [
                 'label' => 'lien vers le site de l\'évènement',
+                'required' => false,
                 'attr' => [
                     'placeholder' => 'https//lien-vers-le-site-de-la-manifestation.com',
                 ],
             ])
             ->add('video', UrlType::class, [
                 'label' => 'lien vers une vidéo youtube de relaxation',
+                'required' => false,
                 'attr' => [
                     'placeholder' => 'https//lien-vers-la-video.com',
                 ],
