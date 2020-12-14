2c1378056438980e91a52c4851adb7e9a6dc098b
calouvts <pascalevetois@gmail.com>
2020-12-14 20:50:30 +0100

    add new fixtures

--- src/DataFixtures/EventFixtures.php
+++ src/DataFixtures/EventFixtures.php
@@ -21,6 +21,14 @@ class EventFixtures extends Fixture
             $event1->setEventlink('https://www.salon-terre-naturelle-orleans.fr/');
             $event1->setCategory('EVENEMENT');
             $event1->setDate(new DateTime());
+            $event1->setEventdate(new DateTime());
+            $event1->setSummary('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+         Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum 
+         ultrices diam. Maecenas ligula massa, varius a, semper congue, ');
+            $event1->setArticle('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+         Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum 
+         ultrices diam. Maecenas ligula massa, varius a, semper congue, ');
+            $event1->setVideo('https://www.youtube.com/watch?v=vnprysVcddk&feature=youtu.be')
             $manager->persist($event1);
         }
 
