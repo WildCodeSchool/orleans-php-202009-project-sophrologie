c84e20d77b31ccc2143b3b061820edf98c86e985
calouvts <pascalevetois@gmail.com>
2020-12-15 16:06:13 +0100

    commit for check github

--- src/Controller/AdminEventController.php
+++ src/Controller/AdminEventController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
  * @Route("/admin/event", name="admin_event_")
@@ -42,14 +43,15 @@ class AdminEventController extends AbstractController
 
     /**
      * @Route("/edit/{id}", name="edit")
+     * @ParamConverter("event", class="App\Entity\Event", options={"mapping": {"id": "id"}})
      */
-    public function edit(Event $event, Request $request, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Event $event): Response
     {
         $form = $this->createForm(EventType::class, $event);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+            $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('admin_event_new');
         }
