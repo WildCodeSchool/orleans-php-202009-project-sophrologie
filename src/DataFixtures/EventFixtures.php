b21f4a56707bb01f9fa6109ec1605e60d123e1c1
calouvts <pascalevetois@gmail.com>
2020-12-15 13:38:40 +0100

    fixtures with faker

--- src/DataFixtures/EventFixtures.php
+++ src/DataFixtures/EventFixtures.php
@@ -6,32 +6,28 @@ use App\Entity\Event;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use DateTime;
+use Faker;
 
 class EventFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+            $faker = Faker\Factory::create('fr_FR');
         for ($i = 1; $i <= 10; $i++) {
-            $event1 = new Event();
-            $event1->setTitle('Salon Terre naturelle et environnement Chapit\'O');
-            $event1->setUrl('https://cdn.pixabay.com/photo/2014/05/21/14/53/pier-349672__340.jpg');
-            $event1->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
-         Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum 
-         ultrices diam. Maecenas ligula massa, varius a, semper congue, ');
-            $event1->setEventlink('https://www.salon-terre-naturelle-orleans.fr/');
-            $event1->setCategory('EVENEMENT');
-            $event1->setDate(new DateTime());
-            $event1->setEventdate(new DateTime());
-            $event1->setSummary('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
-         Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum 
-         ultrices diam. Maecenas ligula massa, varius a, semper congue, ');
-            $event1->setArticle('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
-         Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum 
-         ultrices diam. Maecenas ligula massa, varius a, semper congue, ');
-            $event1->setVideo('https://www.youtube.com/watch?v=vnprysVcddk&feature=youtu.be');
-            $manager->persist($event1);
-        }
+            $event = new Event();
 
+            $event->setTitle($faker->word);
+            $event->setUrl($faker->imageUrl(150, 150));
+            $event->setDescription($faker->text());
+            $event->setEventlink($faker->url());
+            $event->setCategory($faker->text());
+            $event->setDate($faker->dateTime());
+            $event->setEventdate($faker->dateTime());
+            $event->setSummary($faker->text());
+            $event->setArticle($faker->text());
+            $event->setVideo($faker->url());
+            $manager->persist($event);
+        }
         $manager->flush();
     }
 }
