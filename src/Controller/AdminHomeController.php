352a17e916f6dd84b2154cbf9efbb41bb2b1fb7b
KevinVanphavong <kevin45van@hotmail.fr>
2020-12-16 14:43:38 +0100

    adding the edit part for speciality

--- src/Controller/AdminHomeController.php
+++ src/Controller/AdminHomeController.php
@@ -25,25 +25,4 @@ class AdminHomeController extends AbstractController
     {
         return $this->render('admin/layoutAdmin.html.twig');
     }
-
-    /**
-     * @param Request $request
-     * @return Response
-     * @Route("/speciality/new",  name="new_speciality")
-     */
-    public function newSpeciality(Request $request): Response
-    {
-        $speciality = new Speciality();
-        $form = $this->createForm(SpecialityType::class, $speciality);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($speciality);
-            $entityManager->flush();
-            return $this->redirectToRoute('home');
-        }
-        return $this->render('speciality/new.html.twig', [
-            "form" => $form->createView(),
-        ]);
-    }
 }
