44df6cd9dce3f8a8a7e66f692f36e7d26d020b4f
KevinVanphavong <kevin45van@hotmail.fr>
2020-12-17 12:41:09 +0100

    adding index for speciality in admin

--- src/Controller/AdminSpecialityController.php
+++ src/Controller/AdminSpecialityController.php
@@ -17,6 +17,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminSpecialityController extends AbstractController
 {
     /**
+     * @Route("/index", name="index")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        $specialities = $this->getDoctrine()
+            ->getRepository(Speciality::class)
+            ->findAll();
+
+        return $this->render(
+            'speciality/indexAdmin.html.twig',
+            [
+                'specialities' => $specialities]
+        );
+    }
+
+    /**
      * @param Request $request
      * @return Response
      * @Route("/new",  name="new_speciality")
