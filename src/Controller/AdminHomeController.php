621cc0a2dab472b90f19d9fae72c61b340bc7ab7
cedric-n <cedric.ngondo@gmail.com>
2020-12-17 23:11:34 +0100

    fix conflict

--- src/Controller/AdminHomeController.php
+++ src/Controller/AdminHomeController.php
@@ -2,10 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Speciality;
-use App\Form\SpecialityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,6 +22,5 @@ class AdminHomeController extends AbstractController
     {
 
         return $this->render('admin/index.html.twig');
-
     }
 }
