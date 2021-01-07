bf6ba4849918b5925a77fc5470141ba40e58175e
calouvts <pascalevetois@gmail.com>
2021-01-07 11:20:27 +0100

    last modfi

--- src/Form/SearchEventFormType.php
+++ src/Form/SearchEventFormType.php
@@ -20,7 +20,7 @@ class SearchEventFormType extends AbstractType
                 'choice_label' => 'name',
                 'multiple' => true,
                 'expanded' => true,
-                'label' => 'Selectionner les catégories: ',
+                'label' => 'Selectionner une catégories: ',
                 'label_attr' => [
                     'class' => 'checkbox-inline'
                 ]
