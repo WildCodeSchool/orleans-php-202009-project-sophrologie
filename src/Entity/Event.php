7eaba8b8eb1bd4c6f480722d8dbc659369cd2b96
calouvts <pascalevetois@gmail.com>
2021-01-09 14:17:22 +0100

    add upload file on event

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -109,7 +109,7 @@ class Event
      * @var DateTimeInterface
      */
 
-    private \DateTimeInterface $uploadedAt ;
+    private ?\DateTimeInterface $uploadedAt ;
 
     public function getId(): ?int
     {
