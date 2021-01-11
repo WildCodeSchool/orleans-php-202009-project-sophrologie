d5d300f312a89bb01ca7e21d52568e0ec7c797af
calouvts <pascalevetois@gmail.com>
2021-01-11 11:59:42 +0100

    change requested and add secure route for index

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * Class UserController
@@ -19,12 +20,12 @@ class UserController extends AbstractController
     /**
      *
      * @Route("/index", name="index")
+     * @IsGranted("ROLE_ADMIN")
      */
     public function index(): Response
     {
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+           ]);
     }
 
     /**
