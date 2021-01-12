d5c83c765484d8769bed49261b06b74ac5b49958
cedric-n <cedric.ngondo@gmail.com>
2021-01-12 08:08:05 +0100

    part Add

---
+++ src/Form/TestimonyType.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Testimony;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TestimonyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'attr' => [
+                    'placeholder' => 'Témoignage'
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Témoignage',
+                ],
+            ])
+            ->add('date', DateType::class, [
+                'label' => 'Date du témoignagz',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'required' => false,
+            ])
+            ->add('firtsname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Témoignage'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Témoignage'
+                ]
+            ])
+            ->add('age', IntegerType::class, [
+                'label' => 'Age',
+                'attr' => [
+                    'placeholder' => 'Témoignage'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Testimony::class,
+        ]);
+    }
+}
