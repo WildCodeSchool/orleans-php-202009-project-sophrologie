ee6083f0f265400d9221ac01f813c8bf4bd19d81
bloic <loic.benos@gmail.com>
2021-01-12 15:27:50 +0100

    change request

--- migrations/Version20210111095303.php
+++ migrations/Version20210111095303.php
@@ -21,7 +21,6 @@ final class Version20210111095303 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE price (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, price INT NOT NULL, description VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('DROP TABLE price');
     }
 
     public function down(Schema $schema) : void
