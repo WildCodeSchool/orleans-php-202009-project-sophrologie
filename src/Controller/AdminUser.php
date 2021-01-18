81f0b14f54fc02c6a66385832eace0590d887e30
KevinVanphavong <kevin45van@hotmail.fr>
2021-01-18 15:58:38 +0100

    create link to redirect user

--- src/Controller/AdminUser.php
+++ src/Controller/AdminUser.php
@@ -13,11 +13,12 @@ class AdminUser extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractContr
 {
 
     /**
+     * @param User $user
      * @return Response
      * @IsGranted("ROLE_USER", statusCode=404)
      * @Route("/mon-compte/{firstname}/{lastname}", name="user")
      */
-    public function myAccount(): Response
+    public function myAccount(User $user): Response
     {
         return $this->render('user/account.html.twig');
     }
