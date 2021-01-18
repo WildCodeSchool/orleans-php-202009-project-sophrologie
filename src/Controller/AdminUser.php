001102ef0feb626ef3c4cb994e84e20b3a25f074
KevinVanphavong <kevin45van@hotmail.fr>
2021-01-18 16:47:15 +0100

    adding edit form for user in personnal page

--- src/Controller/AdminUser.php
+++ src/Controller/AdminUser.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -13,12 +15,26 @@ class AdminUser extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractContr
 {
 
     /**
+     * @param Request $request
+     * @param User $user
      * @return Response
      * @IsGranted("ROLE_USER", statusCode=404)
      * @Route("/mon-compte/{firstname}/{lastname}", name="user")
      */
-    public function myAccount(): Response
+    public function myAccount(Request $request, User $user): Response
     {
-        return $this->render('user/account.html.twig');
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash('success', 'Vos modifications ont bien été mises à jour !');
+        }
+
+        return $this->render('user/account.html.twig', [
+            'user' => $user,
+            'formEditUser' => $form->createView(),
+        ]);
     }
 }
