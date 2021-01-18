4ff52658d2964081fad666f3aaa85a0743b9953b
KevinVanphavong <kevin45van@hotmail.fr>
2021-01-18 10:26:36 +0100

    add fixtures

--- migrations/Version20210118092321.php
+++ migrations/Version20210118092321.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210111095303 extends AbstractMigration
+final class Version20210118092321 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,13 +20,16 @@ final class Version20210111095303 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE faq (id INT AUTO_INCREMENT NOT NULL, subject LONGTEXT NOT NULL, feedback LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE price (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, price INT NOT NULL, description VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE testimony (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(100) NOT NULL, description VARCHAR(255) NOT NULL, date DATE NOT NULL, firtsname VARCHAR(75) NOT NULL, lastname VARCHAR(75) DEFAULT NULL, age INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE price (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, price INT NOT NULL, description VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('DROP TABLE faq');
         $this->addSql('DROP TABLE price');
+        $this->addSql('DROP TABLE testimony');
     }
 }
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+
+    private UserPasswordEncoderInterface $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $admin = new User();
+        $admin->setFirstname('Admin');
+        $admin->setLastname('Adminator');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setEmail('amdin@inspireexpire.com');
+        $admin->setPassword($this->passwordEncoder->encodePassword(
+            $admin,
+            'adminpassword'
+        ));
+
+        for ($i = 0; $i > 10; $i++) {
+            $user = new User();
+            $user->setFirstname('User');
+            $user->setLastname('Useritor');
+            $user->setEmail('user' . $i . '@inspireexpire.com');
+            $user->setRoles(['ROLE_ADMIN']);
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'contributorpassword'
+            ));
+
+            $manager->persist($user);
+        }
+
+
+
+        $manager->flush();
+    }
+}
