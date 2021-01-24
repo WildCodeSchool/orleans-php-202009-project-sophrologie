76cfa1f1846dcdb379fb91581700bd88fe4564cd
calouvts <pascalevetois@gmail.com>
2021-01-24 17:25:44 +0100

    index logo séances

--- src/Controller/CollectiveSessionController.php
+++ src/Controller/CollectiveSessionController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Repository\LogoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Logo;
 
 /**
  * @Route("/collective/session", name="collective_session_")
@@ -14,9 +16,10 @@ class CollectiveSessionController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(LogoRepository $logoRepository): Response
     {
         return $this->render('collective_session/index.html.twig', [
+            'logos' => $logoRepository->findAll(),
         ]);
     }
 }
