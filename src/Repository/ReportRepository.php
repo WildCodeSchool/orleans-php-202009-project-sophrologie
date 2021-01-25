4b98a2ff6842ed2d8cdd9ff2296f61bb73b1317c
KevinVanphavong <kevin45van@hotmail.fr>
2021-01-25 21:47:59 +0100

    adding form, controler, repo method

--- src/Repository/ReportRepository.php
+++ src/Repository/ReportRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Report;
+use App\Entity\SearchAdminReports;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -17,6 +18,26 @@ class ReportRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Report::class);
+    }
+
+    public function findWithFilterUsers(SearchAdminReports $search): ?array
+    {
+        if ($search->getUserSelected()) {
+            $query = $this->createQueryBuilder('r')
+                ->andWhere('r.patient = :patient')
+                ->setParameter('patient', $search->getUserSelected());
+            return $query->getQuery()->getResult();
+        }
+
+        return $query = $this->findAllDesc();
+    }
+
+    public function findAllDesc(): array
+    {
+        return $this->createQueryBuilder('r')
+            ->orderBy('r.id', 'DESC')
+            ->getQuery()
+            ->getResult();
     }
 
     // /**
